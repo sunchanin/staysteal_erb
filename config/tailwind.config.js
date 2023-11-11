cbd912d607789c7784b38043a1e9c7f8d4f4de78
chanin <14chaninkrewkerdmee@gmail.com>
2023-11-11 23:48:58 +0700

    sunsun
    sunsun

---
+++ config/tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
